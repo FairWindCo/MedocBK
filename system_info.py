f43defd747e0845e92d11c13fc2a50221c378597
FairWind <sergey.manenok@gmail.com>
2022-03-31 19:46:13 +0300

    create system info utility add send backup info to server

--- system_info.py
+++ system_info.py
@@ -1,24 +1,293 @@
 import clr
+
 clr.AddReference("System.ServiceProcess")
 clr.AddReference("IronPython")
 clr.AddReference("StdLib")
 clr.AddReference("System.Management")
 
 from System.Management import ManagementObjectSearcher
+from System.ServiceProcess import ServiceController, ServiceControllerStatus
+from Microsoft.Win32 import Registry
+import re
+from datetime import datetime
+import platform
+import json
+import os
+import base64
+from System.Net import Dns
+from System.Net.Sockets import AddressFamily
+from rsa import encrypt, PublicKey
+import argparse
+
+
+def clear_name(name, ver):
+    if name:
+        if ver:
+            # pattern = '[\\s]*[-]?[\\s]*' + ver
+            pattern = r'[\s-]*' + ver + r'[.0-9\s]*'
+            name = re.sub(pattern, '', name)
+        name = name.replace('False', '').strip()
+    return name
+
+
+def form_soft(name, ver, install):
+    # return clear_name(name, ver), ver, datetime.strptime(install, '%Y%m%d') if install else None
+    return clear_name(name, ver), ver, install
+
+
+def get_wmi_soft():
+    mos = ManagementObjectSearcher("select Name, Version, InstallDate from Win32_Product")
+    if mos:
+        return [form_soft(mo['Name'], mo['Version'], mo['InstallDate']) for mo in mos.Get()]
+    return []
+
+
+def get_wmi_futures():
+    try:
+        mos = ManagementObjectSearcher("select Name from Win32_ServerFeature")
+        if mos:
+            return [mo['Name'] for mo in mos.Get()]
+    except Exception:
+        pass
+    return []
+
+
+def get_reg_key(current_user=False, is64=False):
+    if is64:
+        key = "SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+    else:
+        key = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+    if current_user:
+        return Registry.LocalMachine.OpenSubKey(key)
+    else:
+        return Registry.LocalMachine.OpenSubKey(key)
+
+
+def get_display_name(parent_key, current_name):
+    key = parent_key.OpenSubKey(current_name)
+    if key:
+        name = key.GetValue("DisplayName")
+        ver = key.GetValue("DisplayVersion")
+        installed = key.GetValue("InstallDate")
+        if name:
+            name = clear_name(name, ver)
+        else:
+            default_val = key.GetValue('')
+            name = default_val if default_val else current_name
+        return name, ver, installed
+    else:
+        return current_name, None, None
+
+
+def list_sub_keys(key):
+    if key:
+        return [get_display_name(key, k) for k in key.GetSubKeyNames()]
+    else:
+        return []
+
+
+def listing_sub_keys(key):
+    if key:
+        return key.GetSubKeyNames()
+    else:
+        return []
+
+
+def convert_to_dict(info_list, info_dict=None):
+    if info_dict is None:
+        info_dict = {}
+    if info_list:
+        for info in info_list:
+            info_dict[info[0]] = info
+    return info_dict
+
+
+def get_installed_soft():
+    paths1 = list_sub_keys(get_reg_key(False, False))
+    paths2 = list_sub_keys(get_reg_key(True, False))
+    paths3 = list_sub_keys(get_reg_key(False, True))
+    paths4 = list_sub_keys(get_reg_key(True, True))
+    soft = convert_to_dict(paths1, None)
+    soft = convert_to_dict(paths2, soft)
+    soft = convert_to_dict(paths3, soft)
+    soft = convert_to_dict(paths4, soft)
+    return soft
+
+
+def get_services():
+    services = ServiceController.GetServices()
+    return [service.DisplayName for service in services
+            if service.Status == ServiceControllerStatus.Running]
+
+
+def get_system_info(log=False):
+    objects_names = [
+        "Win32_OperatingSystem",
+        "Win32_ComputerSystem",
+        "Win32_DiskDrive",
+        "Win32_Processor",
+    ]
+    result = {}
+    for name in objects_names:
+        result[name] = {
+            'infos': [],
+            'count': 0
+        }
+        mos = ManagementObjectSearcher("select * from " + name)
+        count = 0
+        for mo in mos.Get():
+            info = {}
+            for prop in mo.Properties:
+                if log:
+                    print name, prop.Name, prop.Value
+                info[prop.Name] = prop.Value
+            result[name]['infos'].append(info)
+            count += 1
+        result[name]['count'] = count
+
+    return result
+
+
+def form_host_info_json(small_info=False):
+    info = get_system_info()
+    result = {
+        'host': platform.node(),
+    }
+    if not small_info:
+        result.update(
+            {'SystemFamily': info['Win32_ComputerSystem']['infos'][0]['SystemFamily'],
+             'Model': info['Win32_ComputerSystem']['infos'][0]['Model'],
+             'Domain': info['Win32_ComputerSystem']['infos'][0]['Domain'],
+             'Manufacturer': info['Win32_ComputerSystem']['infos'][0]['Manufacturer'],
+             'TotalPhysicalMemory': str(info['Win32_ComputerSystem']['infos'][0]['TotalPhysicalMemory']),
+             'NumberOfProcessors': int(info['Win32_ComputerSystem']['infos'][0]['NumberOfProcessors']),
+             'Version': info['Win32_OperatingSystem']['infos'][0]['Version'],
+             'BuildNumber': info['Win32_OperatingSystem']['infos'][0]['BuildNumber'],
+             'InstallDate': info['Win32_OperatingSystem']['infos'][0]['InstallDate'],
+             'OSArchitecture': info['Win32_OperatingSystem']['infos'][0]['OSArchitecture'],
+             'hdd_count': info['Win32_DiskDrive']['count'],
+             'cpu_count': info['Win32_Processor']['count'],
+             'hdd_info': [],
+             'cpu_info': []
+             }
+        )
+
+    for hdd_info in info['Win32_DiskDrive']['infos']:
+        result['hdd_info'].append({
+            'model': hdd_info['Model'],
+            'size': int(hdd_info['Size'])
+        })
+    for cpu_info in info['Win32_Processor']['infos']:
+        result['cpu_info'].append({
+            'model': cpu_info['Name'],
+            'ThreadCount': int(cpu_info['ThreadCount']),
+            'NumberOfCores': int(cpu_info['NumberOfCores']),
+        })
+
+    return result
+
+
+def get_ip_list():
+    host = Dns.GetHostEntry(Dns.GetHostName())
+    return [ip.ToString() for ip in host.AddressList if ip.AddressFamily == AddressFamily.InterNetwork]
+
+
+def send_request(message, url, csrf):
+    import urllib2
+    import urllib
+    post = urllib.quote(message)
+    req = urllib2.Request(url, post)
+    req.add_header('X-CSRFToken', csrf)
+    req.add_header('Cookie', 'csrftoken={}'.format(csrf))
+    response = urllib2.urlopen(req)
+    return response.read()
+
+
+def get_request(url):
+    import urllib2
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    text = response.read()
+    obj_dict = json.loads(text)
+    return obj_dict.get('csrf', None)
+
+
+def load_key(key_path):
+    f = open(key_path, 'rb')
+    try:
+        key = f.read()
+        return PublicKey.load_pkcs1(key)
+    finally:
+        f.close()
+
+
+def encrypt_message(info, state_error=False, config=None):
+    if config is None:
+        config = {}
+    PUBLIC_KEY_FILE = config.get('public_key', None)
+    if PUBLIC_KEY_FILE and os.path.exists(PUBLIC_KEY_FILE) and os.path.isfile(PUBLIC_KEY_FILE):
+        public_key = load_key(PUBLIC_KEY_FILE)
+        host = platform.node()
+        key = base64.b64encode(encrypt(host + datetime.now().strftime("%d%m%y%H%M%S"), pub_key=public_key))
+        info['key'] = key
+        print info
+        mes = json.dumps(info)
+        return mes
+    return None
+
+
+def get_config(config_file='config.json'):
+    json_data = open(config_file).read()
+    config = json.loads(json_data)
+    return config
+
+
+def log_message(info, config=None):
+    if config is None:
+        config = {}
+    url_token = config.get('token_url', 'http://127.0.0.1:8000/token')
+    url_special = config.get('special_url', 'http://127.0.0.1:8000/host_info_update')
+    csrf = get_request(url_token)
+    mes = encrypt_message(info, False, config)
+    send_request(mes, url_special, csrf)
 
 
 if __name__ == "__main__":
+    # for soft in get_wmi_soft():
+    #     print soft
 
-    objects_names = ["Win32_OperatingSystem",
-                     "Win32_ComputerSystem",
-                     "Win32_DiskDrive",
-                     "Win32_Processor",
-                     # "Win32_ProgramGroup",
-                     # "Win32_SystemDevices",
-                     # "Win32_StartupCommand"
-                     ]
-    for name in objects_names:
-        mos = ManagementObjectSearcher("select * from " + name)
-        for mo in mos.Get():
-            for prop in mo.Properties:
-                print "{} \t\t\t {} \t\t\t\t\t {}".format(name, prop.Name, prop.Value)
+    # for ipy_path in get_installed_soft().values():
+    #     print ipy_path
+
+    # get_services()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--soft', dest='soft', action='store_true')
+    parser.add_argument('-f', dest='save')
+    parser.add_argument('-p', dest='echo')
+    parser.add_argument('-s', dest='skip')
+
+    arguments = parser.parse_args()
+    info = form_host_info_json(arguments.soft)
+
+    info['services'] = get_services()
+
+    info['soft'] = [{
+        'name': soft[0],
+        'version': soft[1],
+        'installed': soft[2]
+    } for soft in get_wmi_soft()]
+
+    info['ip'] = get_ip_list()
+    info['futures'] = get_wmi_futures()
+    config = get_config()
+
+    if arguments.save:
+        f = open(arguments.save, 'wt')
+        try:
+            f.write(json.dumps(config))
+        finally:
+            f.close()
+    if not arguments.skip:
+        log_message(info, config)
+    if arguments.echo:
+        print info
--- win_test.py
+++ win_test.py
@@ -1,5 +1,6 @@
 #  & 'C:\Program Files\IronPython 2.7\ipyc.exe' /main:win_test.py /platform:x86 /embed /standalone /target:exe
 import clr
+
 clr.AddReference("System.ServiceProcess")
 clr.AddReference("IronPython")
 clr.AddReference("StdLib")
@@ -14,32 +15,65 @@ import time
 import platform
 import shutil
 from datetime import datetime
+from zipfile import ZipFile
+from rsa import encrypt, PublicKey
+import base64
+import re
+import urllib
+
+
 
 def Main(args):
     json_data = open('config.json').read()
     config = json.loads(json_data)
 
-    SMTP_SERVER = config['server']
-    SMTP_PORT = config['port']
-    FROM_MAIL = config['from_mail']
-    TO_MAIL = config['to_mail']
-    SERVICE = config['medoc_service']
-    WINRAR = config['winrar_path']
+    WINRAR = config.get('winrar_path', None)
     DB_FILE = config['db_file']
-    TMP_BKP = config['db_path']
-    BKP_FORMAT = config['bkp_file_format']
-    BKP_PATH = config['bkp_path']
-    SAFE_TIME = config['safe_time']
-    SERVICE_REACTION = config['service_reaction']
 
+    BKP_PATH = config.get('bkp_path', [])
+    SAFE_TIME = config.get('safe_time', 0)
+    REMOVE_OLD_FILE = config.get('remove_old_file', True)
+
+    if config.get('ignore_service', False):
+        SERVICE = None
+    else:
+        SERVICE = config.get('medoc_service', None)
+    SERVICE_REACTION = config.get('service_reaction', 60)
+
+    def load_key(key_path):
+        f = open(key_path, 'rb')
+        try:
+            key = f.read()
+            return PublicKey.load_pkcs1(key)
+        finally:
+            f.close()
+
+    def encrypt_message(message, state_error=False):
+        PUBLIC_KEY_FILE = config.get('public_key', None)
+        if PUBLIC_KEY_FILE and os.path.exists(PUBLIC_KEY_FILE) and os.path.isfile(PUBLIC_KEY_FILE):
+            public_key = load_key(PUBLIC_KEY_FILE)
+            host = platform.node()
+            key = base64.b64encode(encrypt(host + datetime.now().strftime("%d%m%y%H%M%S"), pub_key=public_key))
+            mes = json.dumps({
+                'host': host,
+                'message': message,
+                'time': time.ctime(),
+                'is_error': state_error,
+                'key': key
+            })
+            return mes
+        return None
 
     def send_mail(message):
+        SMTP_SERVER = config.get('server', '127.0.0.1')
+        SMTP_PORT = config.get('port', 25)
+        FROM_MAIL = config.get('from_mail', '')
+        TO_MAIL = config.get('to_mail', "bspd@erc.ua")
         try:
             smtp = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
             smtp.sendmail(FROM_MAIL, TO_MAIL, message)
         except Exception:
             pass
-
 
     def get_full_paths(db_path):
         result = []
@@ -54,44 +88,111 @@ def Main(args):
                         result.append(spath)
         return result
 
+    def archive_file_rar(path_to_archive, dst_path, winrar_path):
+        exec_rar = 0
+        for file_path in path_to_archive:
+            exec_rar += (1 if subprocess.call(
+                [winrar_path, 'a', '-dh', dst_path, file_path]) == 0 else 0)
+        if exec_rar < len(path_list):
+            send_mail("NOT ALL FILE ARCHIVED")
 
-    if os.path.exists(TMP_BKP) and os.path.isdir(TMP_BKP):
-        # check service
-        service = ServiceController(SERVICE)
-        if service.Status == ServiceControllerStatus.Running:
-            print "Try stop service"
-            service.Stop()
-            time.sleep(SERVICE_REACTION)
-            service = ServiceController(SERVICE)
-            print service.Status
-        host_name = platform.node()
-        date_str = datetime.now().strftime("%d%m%y")
-        s_path = os.path.join(TMP_BKP, BKP_FORMAT.format(host_name, date_str))
-
-        if service.Status != ServiceControllerStatus.Stopped:
-            send_mail("Service {} is not stopped".format(SERVICE))
-            sys.exit(-1)
-
-        path_list = get_full_paths(DB_FILE)
-        if path_list:
-            exec_rar = 0
-            for file_path in path_list:
-                exec_rar += (1 if subprocess.call(
-                    [WINRAR, 'a', '-dh', s_path, file_path]) == 0 else 0)
-            if exec_rar < len(path_list):
-                send_mail("NOT ALL FILE ARCHIVED")
-        else:
-            send_mail("NO DB FILE")
+    def archive_file_zip(path_to_archive, dst_path):
+        ZIP_COMPRESSION = config.get('zip_compression', 8)
         try:
-            service.Start()
-            time.sleep(SERVICE_REACTION)
-            service = ServiceController(SERVICE)
-            if service.Status != ServiceControllerStatus.Running:
+            zipObj = ZipFile(dst_path, 'w', compression=ZIP_COMPRESSION, allowZip64=True)
+            try:
+                for file_path in path_to_archive:
+                    zipObj.write(file_path)
+            finally:
+                zipObj.close()
+        except Exception as e:
+            send_mail("ZIP ERROR " + e.message)
+
+    def stop_service(service_name, service_wait_time=60):
+        if service_name:
+            service = ServiceController(service_name)
+            if service.Status == ServiceControllerStatus.Running:
+                print "Try stop service"
+                service.Stop()
+                time.sleep(service_wait_time)
+                service = ServiceController(service_name)
+                print service.Status
+            if service.Status != ServiceControllerStatus.Stopped:
+                send_mail("Service {} is not stopped".format(service_name))
+                sys.exit(-1)
+
+    def start_service(service_name, service_wait_time=60):
+        if service_name:
+            try:
+                service = ServiceController(service_name)
+                service.Start()
+                time.sleep(service_wait_time)
+                service = ServiceController(service_name)
+                if service.Status != ServiceControllerStatus.Running:
+                    send_mail("Service not started")
+            except Exception:
                 send_mail("Service not started")
 
-        except Exception:
-            send_mail("Service not started")
+    def archive_file(path_to_archive, dst_path, winrar_path):
+        if path_to_archive:
+            if winrar_path:
+                archive_file_rar(path_to_archive, dst_path, winrar_path)
+            else:
+                archive_file_zip(path_to_archive, dst_path)
+        else:
+            send_mail("NO DB FILE")
+
+    def get_result_file_name(winrar_path, is_template=False):
+        host_name = platform.node()
+        BKP_FORMAT = config.get('bkp_file_format', '{}zvit{}.rar')
+        if not winrar_path:
+            BKP_FORMAT = BKP_FORMAT.replace('.rar', '.zip')
+        date_str = '[0-9]{6}\\' if is_template else datetime.now().strftime("%d%m%y")
+        return BKP_FORMAT.format(host_name, date_str)
+
+    def get_result_path(winrar_path):
+        TMP_BKP = config.get('db_path', '.')
+        bkp_file_name = get_result_file_name(winrar_path, False)
+        if os.path.exists(TMP_BKP) and os.path.isdir(TMP_BKP):
+            s_path = os.path.join(TMP_BKP, bkp_file_name)
+            print s_path
+            return s_path
+        return None
+
+    def send_request(message, url, csrf):
+        import urllib2
+        post = urllib.quote(message)
+        req = urllib2.Request(url, post)
+        req.add_header('X-CSRFToken', csrf)
+        req.add_header('Cookie', 'csrftoken={}'.format(csrf))
+        response = urllib2.urlopen(req)
+        return response.read()
+
+    def get_request(url):
+        import urllib2
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        text = response.read()
+        obj_dict = json.loads(text)
+        return obj_dict.get('csrf', None)
+
+    def log_message(message):
+        url_token = config.get('token_url', 'http://127.0.0.1:8000/token')
+        url_special = config.get('special_url', 'http://127.0.0.1:8000/special')
+        csrf = get_request(url_token)
+        mes = encrypt_message(message)
+        send_request(mes, url_special, csrf)
+
+    log_message("START")
+    s_path = get_result_path(WINRAR)
+    if s_path:
+        stop_service(SERVICE, SERVICE_REACTION)
+        path_list = get_full_paths(DB_FILE)
+        archive_file(path_list, s_path, WINRAR)
+        start_service(SERVICE, SERVICE_REACTION)
+
         d_name = os.path.basename(s_path)
+        template = re.compile(get_result_file_name(WINRAR, True))
         file_copy = 0
         for path in BKP_PATH:
             d_path = os.path.join(path, d_name)
@@ -102,16 +203,20 @@ def Main(args):
                 send_mail("Can`t copy to {}".format(d_path))
             days_before = (time.time() - SAFE_TIME * 86400)
 
-            for f in os.listdir(path):
-                file_path = os.path.join(path, f)
-                if f.startswith(host_name) and os.stat(file_path).st_mtime < days_before:
-                    os.remove(file_path)
+            if REMOVE_OLD_FILE:
+                for f in os.listdir(path):
+                    file_path = os.path.join(path, f)
+                    if template.match(f) and os.stat(file_path).st_mtime < days_before:
+                        os.remove(file_path)
 
         if file_copy > 0:
             os.remove(s_path)
+
+        log_message("WELL DONE")
     else:
         send_mail("NO TMP PATH")
         exit(-2)
+
 
 if __name__ == "__main__":
     Main(sys.argv[1:])
